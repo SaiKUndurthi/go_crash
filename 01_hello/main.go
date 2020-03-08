c05daa16429aba27aba68a7ec54b8aae3a260cb6
sai-u <saiu@jfrog.com>
2020-03-08 14:18:45 -0700

    Variables

--- 01_hello/main.go
+++ 01_hello/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var name = "Sai"
+	var num = 3
+	var isCool = true
+	const notCool = false
+	// notCool = true ---> can't be assigned as it's a const
+	name1, email := "Short notation  only inside a function", "test@mail.com"
+	fmt.Println("Hello, " + name + "!!!")
+	fmt.Println(name1, email)
+	fmt.Printf("%T\n", num)
+	fmt.Printf("%T\n", isCool)
 }
